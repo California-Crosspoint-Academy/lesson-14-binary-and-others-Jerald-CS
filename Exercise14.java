ebfa6c1d17e49bbf79252b401adcb3a71052e20a
Jerald-CS <jeraldhuang26@crosspointacademy.org>
2025-10-01 20:19:17 -0700

    did questions

---
+++ Exercise14.java
@@ -0,0 +1,43 @@
+//Author Jerald Huang
+//Date 2025-10-01
+
+public class Exercise14{
+    public static void main(String[] args){
+        //question 9
+        int a = 0x5C3B;
+        System.out.println(a);
+
+        //question 11
+        //111011110 binary (just divide 2)
+
+        //question 12
+        //162E hex  (divide by 16)
+
+        //question 13
+        //13056 oct (divide by 8)
+
+        //question 19 
+        //it is written as 0x10 16/16 
+
+        //question 20
+        //It's base is 0 and 1 
+
+        //question 21
+        //11010110
+
+        //question 22
+        //10100010
+
+        //question 23 
+        //the joke is 10 represents 2 so it means only 2 people understand binary
+
+        //question 24
+        String s = "1011"; //example number in base b
+        int b = 2;          //example base
+        int i = Integer.parseInt(s, b);
+
+        //question 30
+        //prints 9
+
+    }
+}
